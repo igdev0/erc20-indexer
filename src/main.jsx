da0d315ec76ea2c8c64709316abed6276c0bbed8
igdev <dorultanianos@gmail.com>
2025-04-17 11:28:17 +0300

    upgrade to latest version of chakra

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import {Provider} from './components/ui/provider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
 )
